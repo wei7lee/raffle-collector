06417933beb8d6db0d6be87b0c7d889217e45d00
weiqilee <weiqilee777@gmail.com>
2022-12-18 19:57:25 +0800

    first commit

---
+++ src/main/kotlin/com/wei7lee/nft/raffle/collector/RaffleCollectorApplication.kt
@@ -0,0 +1,11 @@
+package com.wei7lee.nft.raffle.collector
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RaffleCollectorApplication
+
+fun main(args: Array<String>) {
+	runApplication<RaffleCollectorApplication>(*args)
+}
---
+++ src/test/kotlin/com/wei7lee/nft/raffle/collector/RaffleCollectorApplicationTests.kt
@@ -0,0 +1,12 @@
+package com.wei7lee.nft.raffle.collector
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RaffleCollectorApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+}
